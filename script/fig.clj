5c74af621ff0e65d922fa0d7ddcf55b636dc1566
pbaille <pierrebaille@gmail.com>
2016-03-06 15:36:59 +0100

    fix range selector

--- script/fig.clj
+++ script/fig.clj
@@ -3,7 +3,7 @@
 
 (ra/start-figwheel!
   {:figwheel-options {:css-dirs ["resources/public"]
-                      :server-port 5000}
+                      :server-port 5001}
    :build-ids ["dev"]
    :all-builds
                      [{:id "dev"
--- src/rec/ranges/comp.cljs
+++ src/rec/ranges/comp.cljs
@@ -2,64 +2,70 @@
   (:require-macros [reagent.ratom :refer [reaction]])
   (:require [reagent.core :as r :refer [atom]]))
 
-(defn simple-range [kw {:keys [on-change value]
-                        :or {value 0}
-                        :as opts}]
-  [:div.simplerange {:class (name kw)}
-   [:span (name kw)]
-   [:input (merge {:type "number"
-                   :default-value value
-                   :on-change #(on-change (.. % -target -value))}
-                  (dissoc opts :on-change :value))]])
+(defn simple-range [{:keys [on-change value op]
+                     :or {value 0}
+                     :as opts}]
+  (let [op-str (name op)
+        state (atom {:op op-str :value value})]
+    (fn []
+      [:div.simplerange {:class op-str}
+       [:span op-str]
+       [:input (merge {:type "number"
+                       :default-value value
+                       :on-change #(do
+                                    (swap! state assoc :value (.. % -target -value))
+                                    (on-change @state))}
+                      (dissoc opts :on-change :value :op))]])))
 
-(defn range* [kw {:keys [on-change from to]
-                  :or {from 0 to 0}
+(defn range* [{:keys [on-change value op]
                   :as opts}]
-  (let [state (atom {:from from :to to})
+  (let [op-str (name op)
+        state (atom {:op op-str
+                     :value value})
         on-from-change (fn [from]
-                         (let [to (:to @state)
+                         (let [to (get-in @state [:value :to])
                                move-to? (> from to)]
-                           (swap! state assoc :from from :to (if move-to? from to))
+                           (swap! state assoc :value {:from from :to (if move-to? from to)})
                            (on-change @state)))
         on-to-change (fn [to]
-                       (let [from (:from @state)
+                       (let [from (get-in @state [:value :from])
                              move-from? (> from to)]
-                         (swap! state assoc :from (if move-from? to from) :to to)
-                         (on-change @state)))]
+                         (swap! state assoc :value {:from (if move-from? to from) :to to})
+                         (on-change @state)))
+        cleaned-opts (dissoc opts :on-change :value :op)]
+    (println cleaned-opts)
     (fn []
-      [:div.range {:class (name kw)}
-       [:span (str (name kw) "  ")]
+      [:div.range {:class op-str}
+       [:span (str op-str "  ")]
        [:input (merge {:type "number"
-                       :value (:from @state)
+                       :value (get-in @state [:value :from])
                        :on-change #(on-from-change (.. % -target -value))}
-                      (dissoc opts :on-change :from :to))]
+                      cleaned-opts)]
        [:input (merge {:type "number"
-                       :value (:to @state)
+                       :value (get-in @state [:value :to])
                        :on-change #(on-to-change (.. % -target -value))}
-                      (dissoc opts :on-change :from :to))]])))
+                      cleaned-opts)]])))
 
-(def gt (partial simple-range :>))
-
-(def gte (partial simple-range :>=))
-
-(def lt (partial simple-range :<))
-
-(def lte (partial simple-range :<=))
-
-(def in (partial range* :in))
-
-(def out (partial range* :out))
+(defn gt [opts] (simple-range (merge {:op  :>} opts)))
+(defn gte [opts] (simple-range (merge {:op :>=} opts)))
+(defn lt [opts] (simple-range (merge {:op  :<} opts)))
+(defn lte [opts] (simple-range (merge {:op :<=} opts)))
+(defn in [opts] (range* (merge {:op :in} opts)))
+(defn out [opts] (range* (merge {:op :out} opts)))
 
 (defn- get-default-value [{:keys [min max]} name]
-  (get {">" {:value min}
-        ">=" {:value min}
-        "<" {:value max}
-        "<=" {:value max}
+  (get {">" min
+        ">=" min
+        "<" max
+        "<=" max
         "in" {:from min :to max}
         "out" {:from 0 :to 0}}
        name))
 
-(defn multirange [{:keys [on-change] :as opts}]
+(defn multirange [{:keys [on-change placeholder]
+                   :or {placeholder "add a range"
+                        on-change identity}
+                   :as opts}]
   (let [state (atom {:ranges {}
                      :focus false
                      :value {}})
@@ -69,7 +75,7 @@
         [:div.multirange-container
          (if-not focus?
            [:div.add {:on-click #(swap! state assoc :focus true)}
-            [:span "add a range"]]
+            [:span placeholder]]
            [:div.constructors
             (for [[name component] {">" gt ">=" gte "<" lt "<=" lte "in" in "out" out}]
               (let [sym (gensym)]
@@ -85,13 +91,12 @@
                                           assoc
                                           sym
                                           ^{:key (gensym)}
-                                          [component (merge
-                                                       default-value
-                                                       (assoc opts
-                                                         :on-change
-                                                         (fn [x]
-                                                           (swap! state update :value assoc sym x)
-                                                           (on-change (:value @state)))))])
+                                          [component (assoc opts
+                                                       :value default-value
+                                                       :on-change
+                                                       (fn [x]
+                                                         (swap! state update :value assoc sym x)
+                                                         (on-change (:value @state))))])
                                   (update :value assoc sym (:value default-value default-value)))))
                     (on-change (:value @state)))}
                  name]))])
