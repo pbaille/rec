b3f01c18582ddaad90cc762b7eecd9faa1a142d5
pbaille <pierrebaille@gmail.com>
2016-03-20 19:47:50 +0100

    move comp-selector in his own ns

--- src/rec/sidebar.cljs
+++ src/rec/sidebar.cljs
@@ -6,50 +6,16 @@
             [rec.tags.comp :refer [tags]]
             [rec.multitoggle.comp :refer [multitoggle]]
             [rec.multirangeslider.comp :refer [multi-rangeslider]]
+            [rec.comp-selector.comp :refer [comp-selector]]
             [cljs-time.core :as t]))
 
-;; helpers ----------------------------------------------
-
-(defn comp-selector [{:keys [icon-class comps on-change placeholder]
-                      :or {placeholder "Select a component"}}]
-  (let [state (atom {:comps comps
-                     :selected #{}})
-        selected? #((:selected @state) (key %))]
-    (fn []
-      [:div.comp-selector
-
-       [:div.input-group
-        {:z-index :auto}
-        [:div.input-group-addon
-         [:i.fa {:class icon-class}]]
-        [:div.dropdown.form-control
-         {:style {:cursor :pointer
-                  :z-index :auto}}
-         [:div {:data-toggle "dropdown"}
-          placeholder]
-         [:ul.dropdown-menu
-          {:style {:z-index 10}}
-          (doall
-            (for [[name* _] (filter (comp not selected?) (:comps @state))]
-              ^{:key (gensym)}
-              [:li {:style {:height "30px"
-                            :line-height :30px
-                            :padding-left :15px}
-                    :on-click #(swap! state update :selected conj name*)}
-               (name name*)]))]]]
-       [:div.selected-comps
-        {:style {:padding :20px}}
-        (doall
-          (for [[name comp] (filter selected? (:comps @state))]
-            (assoc-in comp [1 :on-delete] #(swap! state update :selected disj name))))]])))
+;; sub components ------------------------------------------
 
 (defn label [s]
   [:div
    {:style {:font-size :20px
             :padding-bottom :10px}}
    s])
-
-;; sub components ------------------------------------------
 
 (defn period [{:keys [from to on-change]
                :or {from (t/today-at-midnight)
